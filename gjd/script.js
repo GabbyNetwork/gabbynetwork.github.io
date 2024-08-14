beff441ad18b0ab3d5c05a9175a1046703a0f059
OnlyCreativeNetwork <internogjd@gmail.com>
2024-08-14 03:55:50 +0200

    Update script.js

--- gjd/script.js
+++ gjd/script.js
@@ -1,1 +1,22 @@
+// Cambio colore dei link all'hover
+const navLinks = document.querySelectorAll('nav ul li a');
 
+navLinks.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        link.style.color = '#ff99cc';
+        link.style.transition = 'color 0.3s ease';
+        link.classList.add('bounce');
+    });
+
+    link.addEventListener('mouseout', () => {
+        link.style.color = 'white';
+        link.classList.remove('bounce');
+    });
+});
+
+// Aggiungere la classe bounce per animazioni al passaggio del mouse
+navLinks.forEach(link => {
+    link.addEventListener('animationend', () => {
+        link.classList.remove('bounce');
+    });
+});
